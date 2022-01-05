a4bc2154ae95a1ee46874f03075bdf4bf49ce329
王 <22379099@qq.com>
2022-01-05 09:50:46 +0800

    修改接口参数

--- src/WWB.SDK.ArcFaceApi/Models/FaceSaveRequest.cs
+++ src/WWB.SDK.ArcFaceApi/Models/FaceSaveRequest.cs
@@ -10,13 +10,30 @@ namespace WWB.SDK.ArcFaceApi.Models
         /// <summary>
         /// 人脸特征标识
         /// </summary>
-        [Required]
+        [Required(ErrorMessage = "人脸特征标识不能为空")]
         public string Id { get; set; }
+
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        [Required(ErrorMessage = "姓名不能为空")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 手机号
+        /// </summary>
+        [Required(ErrorMessage = "手机号不能为空")]
+        public string Phone { get; set; }
 
         /// <summary>
         /// 人脸特征
         /// </summary>
-        [Required]
-        public string FeatureBase64Str { get; set; }
+        [Required(ErrorMessage = "base64人脸特征不能为空")]
+        public string Feature { get; set; }
+
+        /// <summary>
+        /// Url
+        /// </summary>
+        public string Url { get; set; }
     }
 }
